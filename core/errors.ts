79762a53af2bdcb83cd6e3f46028e35fd2b1424c
Konstantin Tarkus <koistya@gmail.com>
2022-05-26 19:37:10 +0300

    refactor: Switch from Service Worker to ES Modules syntax

--- core/errors.ts
+++ core/errors.ts
@@ -1,12 +1,15 @@
 /* SPDX-FileCopyrightText: 2020-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-export class HttpError extends Error {
-  readonly status: number;
-
-  constructor(status: number, message: string) {
-    super(message);
-    this.status = status;
-    Object.setPrototypeOf(this, Error.prototype);
-  }
+export function handleError<Env = unknown>(
+  handler: ExportedHandlerFetchHandler<Env>
+): ExportedHandlerFetchHandler<Env> {
+  return function (req, res, ctx) {
+    try {
+      return handler(req, res, ctx);
+    } catch (err) {
+      const message = err instanceof Error ? err.message : "Application error";
+      return new Response(message, { status: 500 });
+    }
+  };
 }
--- site/index.ts
+++ site/index.ts
@@ -1,21 +1,17 @@
 /* SPDX-FileCopyrightText: 2020-present Kriasoft */
 /* SPDX-License-Identifier: MIT */
 
-async function handleRequest(req: Request): Promise<Response> {
-  const url = new URL(req.url);
+import { handleError } from "core";
 
-  url.protocol = "https:";
-  url.hostname = "welcome.developers.workers.dev";
+export default {
+  fetch: handleError((req) => {
+    const url = new URL(req.url);
 
-  return fetch(url.toString(), req);
-}
+    url.protocol = "https:";
+    url.hostname = "welcome.developers.workers.dev";
 
-function handleError(err: Error): Response {
-  return new Response(err.message, { status: 500 });
-}
+    return new Response("OK", { status: 200 });
 
-addEventListener("fetch", function (event) {
-  event.respondWith(handleRequest(event.request).catch(handleError));
-});
-
-export {};
+    return fetch(url.toString(), req);
+  }),
+} as ExportedHandler;
