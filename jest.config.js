ca9a320065ee3c66cc6020bf665c5ef870bc13b9
Konstantin Tarkus <koistya@gmail.com>
2022-06-01 20:28:40 +0300

    feat: Add `jest-environment-miniflare` to Jest config

---
+++ jest.config.js
@@ -0,0 +1,54 @@
+/* SPDX-FileCopyrightText: 2020-present Kriasoft */
+/* SPDX-License-Identifier: MIT */
+
+import envars from "envars";
+
+/**
+ * Jest configuration
+ * https://jestjs.io/docs/configuration
+ *
+ * @type {import("@jest/types").Config.InitialOptions}
+ */
+export default {
+  cacheDirectory: ".cache/jest",
+
+  // Miniflare Settings
+  // https://miniflare.dev/get-started/api
+  testEnvironment: "miniflare",
+  testEnvironmentOptions: {
+    bindings: envars.config(),
+    kvNamespaces: [],
+  },
+
+  testPathIgnorePatterns: [
+    "<rootDir>/.cache/",
+    "<rootDir>/.github/",
+    "<rootDir>/.husky/",
+    "<rootDir>/.vscode/",
+    "<rootDir>/.yarn/",
+    "<rootDir>/*/dist/",
+    "<rootDir>/scripts/",
+  ],
+
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node",
+  ],
+
+  modulePathIgnorePatterns: ["<rootDir>/*/dist/"],
+
+  setupFiles: ["<rootDir>/test/setup.ts"],
+
+  transform: {
+    "\\.ts$": "babel-jest",
+  },
+
+  extensionsToTreatAsEsm: [".ts"],
+};
