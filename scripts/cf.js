e68ec993b01041c373d3ce08d2db3e017f12ba72
Konstantin Tarkus <koistya@gmail.com>
2022-06-01 23:42:12 +0300

    chore: Wrangler CLI wrapper improvements

--- scripts/cf.js
+++ scripts/cf.js
@@ -3,43 +3,46 @@
 
 import envars from "envars";
 import { execa as $ } from "execa";
-import { argv, fs, path } from "zx";
+import { argv, fs } from "zx";
 
 // Load environment variables from the `/env/.{envName}.env` file
 const env = process.env;
 const envName = argv.env ?? "test";
 envars.config({ env: envName });
-env.CF_API_TOKEN = env.CF_API_TOKEN ?? env.CLOUDFLARE_API_TOKEN;
 
 // Change the current working directory to the target workspace
 const [, , , target, ...args] = process.argv;
 
 // Inject environment variables into the `wrangler.toml` file
-const configFile = path.resolve(__dirname, `../${target}/dist/wrangler.toml`);
-const configTemplate = path.resolve(__dirname, `../${target}/wrangler.toml`);
+const configFile = `${target}/dist/wrangler.toml`;
+const configTemplate = `${target}/wrangler.toml`;
 const config = await fs.readFile(configTemplate, "utf-8");
 await fs.writeFile(configFile, replaceEnvVars(config), "utf-8");
 
-// Append the target environment if missing
-if (!args.some((x) => x.startsWith("--env=") || x === "--env" || x === "-e")) {
+// Append the target environment name if missing
+if (
+  ["publish", "secret", "dev", "tail"].includes(args[0] /* command */) &&
+  !args.some((x) => x.startsWith("--env=") || x === "--env" || x === "-e")
+) {
   args.push(`--env=${envName}`);
 }
 
-// Pass the secret values loaded from the environment or *.env file(s)
-if (args[0] === "secret" && args[1] === "put" && env[args[2]]) {
-  const p = $("yarn", ["wrangler", "-c", configFile, ...args], {
-    stdio: ["pipe", "inherit", "inherit"],
-  });
+// Launch Wrangler CLI
+const p = $("yarn", ["wrangler", "-c", configFile, ...args], {
+  stdio: args[0] === "secret" ? ["pipe", "inherit", "inherit"] : "inherit",
+});
 
+// Write secret values to stdin (in order to avoid typing them)
+if (args[0] === "secret" && args[1] === "put" && env[args[2]]) {
   p.stdin.write(env[args[2]]);
   p.stdin.end();
-
-  await p;
-  process.exit();
 }
 
-// Launch Wrangler CLI
-await $("yarn", ["wrangler", "-c", configFile, ...args], { stdio: "inherit" });
+// Suppress the error message from the spawned process
+await p.catch(() => {
+  process.exitCode = process.exitCode ?? 1;
+  return Promise.resolve();
+});
 
 function replaceEnvVars(/** @type {string} */ config) {
   return (
