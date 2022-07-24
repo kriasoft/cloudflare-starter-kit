863b74df820bc7edbddcd41b9f632fed409a7a49
Konstantin Tarkus <koistya@gmail.com>
2022-07-24 17:12:27 +0300

    fix: Minor improvements to Firebase integration (#30)

--- app/state/firebase.ts
+++ app/state/firebase.ts
@@ -5,10 +5,6 @@ import { atom, useRecoilState } from "recoil";
 import * as React from "react";
 import * as firebase from "../core/firebase.js";
 
-type Firebase = typeof firebase;
-
-let promise: Promise<void>;
-
 export const Firebase = atom<Firebase | undefined>({
   key: "Firebase",
   dangerouslyAllowMutability: true,
@@ -19,12 +15,12 @@ export function useFirebase(): Firebase | undefined {
   const [value, set] = useRecoilState(Firebase);
 
   React.useEffect(() => {
-    if (!promise) {
-      promise = import("../core/firebase.js").then((module) => {
-        set(module);
-      });
-    }
+    import("../core/firebase.js").then((module) => {
+      set((prevValue) => (prevValue ? prevValue : module));
+    });
   }, []);
 
   return value;
 }
+
+type Firebase = typeof firebase;
