989d3675429d0871aaa62c4e879769cb9092ca7b
Konstantin Tarkus <koistya@gmail.com>
2022-08-28 17:53:22 +0300

    chore: Upgrade Jest from 28.1.3 to 29.0.1

--- api/index.test.ts
+++ api/index.test.ts
@@ -17,7 +17,7 @@ test("GET /api/people/1", async () => {
     statusText: res.statusText,
     url: res.url,
   }).toMatchInlineSnapshot(`
-    Object {
+    {
       "status": 200,
       "statusText": "OK",
       "url": "https://swapi.dev/api/people/1",
@@ -26,12 +26,12 @@ test("GET /api/people/1", async () => {
 
   const body = await res?.json();
   expect(body).toMatchInlineSnapshot(`
-    Object {
+    {
       "birth_year": "19BBY",
       "created": "2014-12-09T13:50:51.644000Z",
       "edited": "2014-12-20T21:17:56.891000Z",
       "eye_color": "blue",
-      "films": Array [
+      "films": [
         "https://swapi.dev/api/films/1/",
         "https://swapi.dev/api/films/2/",
         "https://swapi.dev/api/films/3/",
@@ -44,13 +44,13 @@ test("GET /api/people/1", async () => {
       "mass": "77",
       "name": "Luke Skywalker",
       "skin_color": "fair",
-      "species": Array [],
-      "starships": Array [
+      "species": [],
+      "starships": [
         "https://swapi.dev/api/starships/12/",
         "https://swapi.dev/api/starships/22/",
       ],
       "url": "https://swapi.dev/api/people/1/",
-      "vehicles": Array [
+      "vehicles": [
         "https://swapi.dev/api/vehicles/14/",
         "https://swapi.dev/api/vehicles/30/",
       ],
